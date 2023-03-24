2bb4c088c16b52856ba438e143bb2e852fd1da12
Irasakumar <irasakumarvijayan@gmail.com>
2023-03-24 10:01:42 +0800

    update version

--- src/main/java/com/kalsym/paymentservice/configs/SwaggerConfig.java
+++ src/main/java/com/kalsym/paymentservice/configs/SwaggerConfig.java
@@ -40,7 +40,7 @@ public class SwaggerConfig {
                 .description("Used to make payment via delivery partners")
                 .termsOfServiceUrl("not added yet")
                 .license("not added yet")
-                .licenseUrl("").version("2.3.3").build();
+                .licenseUrl("").version("2.3.4").build();
     }
 
     @Bean
